382cd97a06b4249b56d34a6c356820e8eda435bc
Sebastian Lancheros <sebastian.lancheros@proper.ai>
2024-09-27 10:01:02 -0500

    Created the project with the basic structure

---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+  <main class="about">This is the about page</main>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
