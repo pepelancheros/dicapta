3285c1c85cc67e98f07250c6ebdb2b68fe8ebfaa
Pepe Lancheros <sebastian.lancheros@proper.ai>
2025-01-14 17:04:58 -0500

    set the redirects in the netlify.toml so the api call goes to strapi and not dicapta

--- server/api/newsletters.get.ts
+++ server/api/newsletters.get.ts
@@ -5,6 +5,7 @@ const API_URL = "https://dicapta-strapi-app-production.up.railway.app/api";
 export default defineEventHandler(async () => {
   try {
     const url = new URL(`${API_URL}/newsletters`);
+    console.log("API URL testingg:", url.toString());
     url.searchParams.append("populate", "*");
     const response = await $fetch(url.toString());
     return response;
